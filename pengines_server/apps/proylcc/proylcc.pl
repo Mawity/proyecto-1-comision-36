4ff6572fdbc22746d1f5763e349df2314edc89e6
Mateo <mateojdenis@gmail.com>
2021-05-20 03:07:52 -0300

    logica en prolog

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -1,31 +1,113 @@
 :- module(proylcc,
 	[  
-		put/4,
+		put/8,
 		gameStatus/2
 	]).
 
 :-use_module(library(lists)).
+:-use_module(library(clpfd)).
+
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% replace(?X, +XIndex, +Y, +Xs, -XsY)
+% replace(+Position, +Content, +Grilla, -GrillaRes)
 %
 
-replace(X, 0, Y, [X|Xs], [Y|Xs]).
+replace([0, C], Pintar, [G|Gs], [Res|Gs]):-
+	pintarEnFila(G, C, Pintar, Res).
 
-replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
-    XIndex > 0,
-    XIndexS is XIndex - 1,
-    replace(X, XIndexS, Y, Xs, XsY).
+replace([F, C], Pintar, [G|Gs], [G|Rs]):-
+	F>0,
+	FAux is F - 1,
+	replace([FAux, C], Pintar, Gs, Rs).
+	
+
+pintarEnFila([F|Fs], 0, Simbolo, [QuedaPintado|Fs]):-
+	(F = Simbolo -> QuedaPintado is ''; QuedaPintado is Simbolo).
+
+%pintarEnFila(['X'|Fs], 0, 'X', [''|Fs]).
+
+%pintarEnFila(['#'|Fs], 0, '#', [''|Fs]).
+
+pintarEnFila([F|Fs], Index, Simbolo, [F|Rs]):-
+	Index>0,
+	IndexAux is Index - 1,
+	pintarEnFila(Fs, IndexAux, Simbolo, Rs).
+
+
+
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% put(+Player, +Position, +Board, -ResBoard)
+% filaEsCorrecta(+Fila, +PistasFilas)
 %
 
-put(Player, Pos, Board, ResBoard):-
-	replace("-", Pos, Player, Board, ResBoard).
+filaEsCorrecta([],[]) :- !.
+
+filaEsCorrecta(Fila, [Part|Rest]) :-
+    Rest \= [],
+    saltearEspaciosIniciales(Fila, Fila2),
+    parteEsCorrecta(Fila2, Fila3, Part),
+    espacioObligatorio(Fila3, Fila4),
+    filaEsCorrecta(Fila4, Rest).
+
+filaEsCorrecta(Fila, [Part|[]]) :-
+    saltearEspaciosIniciales(Fila, Fila2),
+    parteEsCorrecta(Fila2, Fila3, Part),
+    saltearEspaciosIniciales(Fila3, Fila4),
+    filaEsCorrecta(Fila4, []).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% parteEsCorrecta(+Fila, +PistasFilas)
+%
+
+parteEsCorrecta(Fila, Fila, 0).
+
+parteEsCorrecta(['#'|Fila], RestFila, N) :-
+    N > 0,
+    N1 is N - 1,
+    parteEsCorrecta(Fila, RestFila, N1).
+
+
+espacioObligatorio([''|Fila],Fila).
+
+espacioObligatorio(['X'|Fila],Fila).
+
+
+saltearEspaciosIniciales(Fila, Fila).
+
+saltearEspaciosIniciales([''|Fila],RestFila) :-
+    saltearEspaciosIniciales(Fila, RestFila).
+
+saltearEspaciosIniciales(['X'|Fila],RestFila) :-
+    saltearEspaciosIniciales(Fila, RestFila).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% put(+Contenido, +Pos, +PistasFilas, +PistasColumnas, +Grilla, -GrillaRes, -FilaSat, -ColSat)
+%
+
+put(Contenido, [F, C], PistasFilas, PistasColumnas, Grilla, GrillaRes, FilaSat, ColSat):-
+
+	replace([F,C], Contenido, Grilla, GrillaRes),
+
+	nth0(F, Grilla, FilaPos),
+    nth0(F, PistasFilas, PistasFilaPos),
+	(filaEsCorrecta(FilaPos, PistasFilaPos) -> FilaSat = 1; FilaSat = 0),
+
+	transpose(Grilla, GrillaTraspuesta),
+
+    nth0(C, GrillaTraspuesta, ColPos),
+    nth0(C, PistasColumnas, PistasColPos),
+	(filaEsCorrecta(ColPos, PistasColPos) -> ColSat = 1; ColSat = 0).
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -34,7 +116,7 @@ put(Player, Pos, Board, ResBoard):-
 %
 
 gameStatus(Board, Winner):-
-	Lines = [
+	Filas = [
     	[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
@@ -44,7 +126,7 @@ gameStatus(Board, Winner):-
 		[0, 4, 8],
 		[2, 4, 6]
   	],
-  	member([C1, C2, C3], Lines),
+  	member([C1, C2, C3], Filas),
 	nth0(C1, Board, Winner),
 	Winner \= "-",
 	nth0(C2, Board, Winner),
@@ -55,4 +137,6 @@ gameStatus(Board, "?"):-
 	member("-", Board),
 	!.
 
-gameStatus(_Board, "T").
+
+
+%put(#, [0,0], [[2],[],[],[],[]], [3,1], [['',#,#,'',''],[],[],[],[]], GrillaRes, FilaSat, ColSat).
